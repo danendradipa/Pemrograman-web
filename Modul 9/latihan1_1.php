51d8efe9c07d0fb39f0d00bc641247178709fb74
Danendra Dipa <danendrach@gmail.com>
2023-04-18 15:16:07 +0700

    latihan 1 php metode GET

---
+++ Modul 9/latihan1_1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Metode GET</title>
+</head>
+
+<body>
+
+    <form action="<?php $_SERVER['PHP_SELF']; ?>" method="get"> Nama
+        <input type="text" name="nama"> <br>
+        <input type="submit" value="OK">
+    </form>
+
+    <?php
+        if (isset($_GET['nama'])) {
+            echo 'Hello, ' . $_GET['nama'];
+        }
+    ?>
+    
+</body>
+
+</html>
