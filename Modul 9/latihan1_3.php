947226b0d8581e8dca8549cb439df138aaf16092
Danendra Dipa <danendrach@gmail.com>
2023-04-18 15:34:09 +0700

    latihan 1 php identifikasi metode

---
+++ Modul 9/latihan1_3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Identifikasi Metode</title>
+</head>
+
+<body>
+
+    <form action="<?php $_SERVER['PHP_SELF']; ?>" method="get"> Nama
+        <input type="text" name="nama"> <br>
+        <input type="submit" value="OK">
+    </form>
+
+    <?php
+        if (isset($_REQUEST['nama'])) {
+            echo 'Metode, ' . $_SERVER['REQUEST_METHOD'];
+        }
+    ?>
+    
+</body>
+
+</html>
