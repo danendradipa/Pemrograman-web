179acfe77ff297878a18825ae94d597c44c1e721
Danendra Dipa <danendrach@gmail.com>
2023-05-09 21:53:00 +0700

    studi kasus fix

--- Modul 10/logout.php
+++ Modul 10/logout.php
@@ -6,3 +6,4 @@
         echo "<h1>Kamu berhasil logout.</h1>";
         echo "<h2>Klik <a href='studikasus.php'>disini</a> untuk login lagi. <br>Kamu tidak bisa masuk ke <a href='beranda.php'>HOME(Beranda)</a> lagi</h2>";
     }
+?>
