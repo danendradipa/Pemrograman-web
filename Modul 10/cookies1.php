9bfa5e2299e989bd8f3a8f3da563f26d0ca7b05f
Danendra Dipa <danendrach@gmail.com>
2023-05-09 16:21:33 +0700

    membuat cookies fix

--- Modul 10/cookies1.php
+++ Modul 10/cookies1.php
@@ -4,5 +4,5 @@
     setcookie("username", $value);
     setcookie("nama_lengkap", $value2, time()+3600); //expired login 1 jam
     echo "<h2>Ini halaman untuk set cookie</h2>";
-    echo "<h2>Klik <a href='cookies2.php'> disini <a/a>untuk mengecek cookies</h2>";
+    echo "<h2>Klik <a href='cookies2.php'> disini </a>untuk mengecek cookies</h2>";
 ?>
