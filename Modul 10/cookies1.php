a1acbcc5332aa11f988f5761110405ac122cb98d
Danendra Dipa <danendrach@gmail.com>
2023-05-09 16:43:26 +0700

    membuat cookies fix

--- Modul 10/cookies1.php
+++ Modul 10/cookies1.php
@@ -1,7 +1,7 @@
 <?php 
-    $value = 'Dipa';
+    $value1 = 'Dipa';
     $value2 = 'Danendra Dipa';
-    setcookie("username", $value);
+    setcookie("username", $value1);
     setcookie("nama_lengkap", $value2, time()+3600); //expired login 1 jam
     echo "<h2>Ini halaman untuk set cookie</h2>";
     echo "<h2>Klik <a href='cookies2.php'> disini </a>untuk mengecek cookies</h2>";
